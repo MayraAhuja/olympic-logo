d1b0d5e4297cfdb797c1eb3a0038e8a1c47c0fe9
MayraAhuja <90937554+MayraAhuja@users.noreply.github.com>
2021-11-13 15:42:58 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 5;
+ctx.rect(150,143,430,200);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle= "red";
+ctx.lineWidth= 5;
+ctx.rect(143,143,430,200);
+ctx.strokeStyle();
